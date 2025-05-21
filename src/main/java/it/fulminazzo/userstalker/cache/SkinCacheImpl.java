1f31f16e38fc38af94a8af328b666bc097e0e40a
user <fulminazzo21@gmail.com>
2025-05-21 22:47:04 +0200

    Added getJsonFromURL method to get data from URL

--- src/main/java/it/fulminazzo/userstalker/cache/SkinCacheImpl.java
+++ src/main/java/it/fulminazzo/userstalker/cache/SkinCacheImpl.java
@@ -1,7 +1,14 @@
 package it.fulminazzo.userstalker.cache;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
 
 /**
@@ -21,6 +28,35 @@ abstract class SkinCacheImpl implements SkinCache {
     @Override
     public @NotNull String lookupUserSkin(@NotNull String username) throws SkinCacheException {
         return "";
+    }
+
+    /**
+     * Navigates the given URL and returns a {@link JsonObject} from the returned body.
+     *
+     * @param url    the url
+     * @param action the action to display when throwing {@link SkinCacheException}
+     * @return the json object
+     * @throws SkinCacheException a wrapper exception for any error
+     */
+    @NotNull JsonObject getJsonFromURL(final @NotNull String url,
+                                       final @NotNull String action) throws SkinCacheException {
+        try {
+            URL actualUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) actualUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK)
+                throw new SkinCacheException(String.format("Invalid response code when %s: %s", action, responseCode));
+
+            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+            Gson gson = new Gson();
+            return gson.fromJson(reader, JsonObject.class);
+        } catch (MalformedURLException e) {
+            throw new SkinCacheException("Invalid URL provided: " + url);
+        } catch (IOException e) {
+            throw new SkinCacheException(String.format("IOException when %s: %s", action, e.getMessage()));
+        }
     }
 
 }
