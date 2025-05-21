216f7efc160e439860f894a1affafac45c237050
user <fulminazzo21@gmail.com>
2025-05-21 22:08:41 +0200

    Initial commit

---
+++ src/main/java/it/fulminazzo/userstalker/UserStalker.java
@@ -0,0 +1,17 @@
+package it.fulminazzo.userstalker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UserStalker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
