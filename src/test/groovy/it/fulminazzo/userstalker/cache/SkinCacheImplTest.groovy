04bf376274ac8e42b089c0f3a515feea5c03040e
user <fulminazzo21@gmail.com>
2025-05-21 23:27:43 +0200

    Added more tests for lookupUserSkin method

--- src/test/groovy/it/fulminazzo/userstalker/cache/SkinCacheImplTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/cache/SkinCacheImplTest.groovy
@@ -1,5 +1,7 @@
 package it.fulminazzo.userstalker.cache
 
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import spock.lang.Specification
 
 class SkinCacheImplTest extends Specification {
@@ -25,6 +27,24 @@ class SkinCacheImplTest extends Specification {
 
         then:
         !skin.isPresent()
+    }
+
+    def 'test that lookupUserSkin of #jsonObject is as expected'() {
+        given:
+        def skinCache = Spy(TestSkinCache)
+        skinCache.getJsonFromURL(_ as String, _ as String) >> Optional.of(jsonObject)
+
+        when:
+        def skin = skinCache.lookupUserSkin('Notch')
+
+        then:
+        skin.isPresent() == expected
+
+        where:
+        jsonObject                                                                    || expected
+        createData()                                                                  || false
+        createData(new Object(), ['name': 'second'])                                  || false
+        createData(new Object(), ['name': 'textures', 'value': 'skin'], new Object()) || true
     }
 
     def 'test that getJsonFromURL returns valid Json'() {
@@ -76,6 +96,15 @@ class SkinCacheImplTest extends Specification {
         then:
         def e = thrown(SkinCacheException)
         e.message == "Invalid response code when $ACTION: 400"
+    }
+
+    private static JsonObject createData(Object... data) {
+        Gson gson = new Gson()
+        String raw = gson.toJson([
+                'id'        : 'mock-id',
+                'properties': data
+        ])
+        return gson.fromJson(raw, JsonObject)
     }
 
 }
