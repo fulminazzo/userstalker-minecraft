444548209094d930de5045d39de3f90d61dc166c
user <fulminazzo21@gmail.com>
2025-05-21 22:48:56 +0200

    Created mock TestSkinCache for testing purposes

---
+++ src/test/java/it/fulminazzo/userstalker/cache/TestSkinCache.java
@@ -0,0 +1,26 @@
+package it.fulminazzo.userstalker.cache;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class TestSkinCache extends SkinCacheImpl {
+    private final Map<String, String> cache;
+
+    public TestSkinCache() {
+        this.cache = new HashMap<>();
+    }
+
+    @Override
+    public @NotNull Optional<String> findUserSkin(@NotNull String username) {
+        return Optional.ofNullable(cache.get(username));
+    }
+
+    @Override
+    public void storeSkin(@NotNull String username, @NotNull String skin) {
+        cache.put(username, skin);
+    }
+
+}
