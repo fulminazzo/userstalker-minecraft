a9dd5d3f69182b31bf7eef8c8314580afeebcf6c
user <fulminazzo21@gmail.com>
2025-05-22 21:47:32 +0200

    Added Javadoc

--- src/main/java/it/fulminazzo/userstalker/USApiClient.java
+++ src/main/java/it/fulminazzo/userstalker/USApiClient.java
@@ -5,6 +5,7 @@ import it.fulminazzo.userstalker.domain.UserLoginCount;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -19,33 +20,76 @@ public final class USApiClient {
     private final @NotNull String address;
     private final int port;
 
+    /**
+     * Notifies the API of a new user login.
+     *
+     * @param player the user
+     */
     public void notifyUserLogin(final @NotNull Player player) {
 
     }
 
+    /**
+     * Gets the top user logins of all time.
+     *
+     * @return the user logins count
+     */
     public @NotNull List<UserLoginCount> getTopUserLogins() {
         return null;
     }
 
+    /**
+     * Gets the top user logins of the month.
+     *
+     * @return the user logins count
+     */
     public @NotNull List<UserLoginCount> getMonthlyUserLogins() {
         return null;
     }
 
+    /**
+     * Gets the newest user logins.
+     *
+     * @return the user logins
+     */
     public @NotNull List<UserLogin> getNewestUserLogins() {
         return null;
     }
 
+    /**
+     * Gets all the users names that entered the server.
+     *
+     * @return the usernames
+     */
     public @NotNull List<String> getUserNames() {
         return null;
     }
 
+    /**
+     * Gets all the logins of a single user.
+     *
+     * @param username the name of the user
+     * @return the user logins
+     */
     public @NotNull List<UserLogin> getUserLogins(final @NotNull String username) {
         return null;
     }
 
-    @NotNull <T> T query(final @NotNull String method,
-                         final @NotNull String path,
-                         final @NotNull Class<T> convertClass) {
+    /**
+     * Queries the API with the given method and path.
+     * The response is then converted to JSON.
+     *
+     * @param <T>              the type to convert to
+     * @param method           the http method
+     * @param path             the path
+     * @param expectedResponse the expected response
+     * @param convertClass     the convert class
+     * @return the response
+     */
+    @Nullable <T> T query(final @NotNull String method,
+                          final @NotNull String path,
+                          final int expectedResponse,
+                          final @NotNull Class<T> convertClass) {
         return null;
     }
 
