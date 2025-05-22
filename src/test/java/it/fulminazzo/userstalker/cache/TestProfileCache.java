d5b857f3d8e958baf12fc679951f673e410fa7bf
user <fulminazzo21@gmail.com>
2025-05-22 12:28:54 +0200

    Implemented findUserUUID and storeUUID methods

--- src/test/java/it/fulminazzo/userstalker/cache/TestProfileCache.java
+++ src/test/java/it/fulminazzo/userstalker/cache/TestProfileCache.java
@@ -5,23 +5,36 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 public class TestProfileCache extends ProfileCacheImpl {
-    private final Map<String, String> cache;
+    private final Map<String, String> skinCache;
+    private final Map<String, UUID> uuidCache;
 
     public TestProfileCache() {
         super(1);
-        this.cache = new HashMap<>();
+        this.skinCache = new HashMap<>();
+        this.uuidCache = new HashMap<>();
     }
 
     @Override
     public @NotNull Optional<String> findUserSkin(@NotNull String username) {
-        return Optional.ofNullable(cache.get(username));
+        return Optional.ofNullable(skinCache.get(username));
     }
 
     @Override
     public void storeSkin(@NotNull String username, @NotNull String skin) {
-        cache.put(username, skin);
+        skinCache.put(username, skin);
+    }
+
+    @Override
+    public @NotNull Optional<UUID> findUserUUID(@NotNull String username) throws ProfileCacheException {
+        return Optional.ofNullable(uuidCache.get(username));
+    }
+
+    @Override
+    public void storeUUID(@NotNull String username, @NotNull UUID uuid) throws ProfileCacheException {
+        uuidCache.put(username, uuid);
     }
 
 }
