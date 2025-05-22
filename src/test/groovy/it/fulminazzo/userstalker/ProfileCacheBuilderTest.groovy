1e4db6985ab1397cf9e569195df0bdca700511c2
user <fulminazzo21@gmail.com>
2025-05-22 15:29:45 +0200

    Added tests for loadCacheType method

--- src/test/groovy/it/fulminazzo/userstalker/ProfileCacheBuilderTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/ProfileCacheBuilderTest.groovy
@@ -4,7 +4,62 @@ import it.fulminazzo.userstalker.cache.MockFileConfiguration
 import it.fulminazzo.yamlparser.configuration.FileConfiguration
 import spock.lang.Specification
 
+import java.util.logging.Logger
+
 class ProfileCacheBuilderTest extends Specification {
+
+    private final Logger logger = Logger.getLogger('TestUserStalker')
+
+    def 'test that loadCacheType of type #type returns #expected'() {
+        given:
+        def file = mockConfiguration(type, true)
+
+        and:
+        def builder = new ProfileCacheBuilder(logger, file)
+
+        when:
+        def actualType = builder.loadCacheType()
+
+        then:
+        actualType == expected
+
+        where:
+        type   || expected
+        'JSON' || ProfileCacheBuilder.CacheType.JSON
+        'json' || ProfileCacheBuilder.CacheType.JSON
+        'YAML' || ProfileCacheBuilder.CacheType.YAML
+        'TOML' || ProfileCacheBuilder.CacheType.TOML
+        'XML'  || ProfileCacheBuilder.CacheType.XML
+        null   || ProfileCacheBuilder.CacheType.JSON
+    }
+
+    def 'test that loadCacheType with no section returns JSON'() {
+        given:
+        def file = mockConfiguration(null, false)
+
+        and:
+        def builder = new ProfileCacheBuilder(logger, file)
+
+        when:
+        def actualType = builder.loadCacheType()
+
+        then:
+        actualType == ProfileCacheBuilder.CacheType.JSON
+    }
+
+    def 'test that loadCacheType of invalid throws'() {
+        given:
+        def file = mockConfiguration('not-valid', true)
+
+        and:
+        def builder = new ProfileCacheBuilder(logger, file)
+
+        when:
+        builder.loadCacheType()
+
+        then:
+        thrown(IllegalArgumentException)
+    }
 
     def 'test that mock returns correct type'() {
         given:
