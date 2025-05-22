15c5445a0d342c0a6ede2900f7c6b2ebdd6c32ea
user <fulminazzo21@gmail.com>
2025-05-22 17:04:43 +0200

    Added ProfileCacheException wrapped constructor

--- src/main/java/it/fulminazzo/userstalker/cache/ProfileCacheException.java
+++ src/main/java/it/fulminazzo/userstalker/cache/ProfileCacheException.java
@@ -9,12 +9,25 @@ import org.jetbrains.annotations.NotNull;
 public final class ProfileCacheException extends Exception {
 
     /**
-     * Instantiates a new Skin cache exception.
+     * Instantiates a new Profile cache exception.
      *
      * @param message the message
      */
     public ProfileCacheException(final @NotNull String message) {
         super(message);
+    }
+
+    /**
+     * Instantiates a new Profile cache exception.
+     * The final message will be:
+     * <br>
+     * <i>&lt;class-name&gt; when &lt;action&gt;: &lt;exception-message&gt;</i>
+     *
+     * @param action the action
+     * @param cause  the cause
+     */
+    public ProfileCacheException(final @NotNull String action, final @NotNull Throwable cause) {
+        super(String.format("%s when %s: %s", cause.getClass().getSimpleName(), action, cause.getMessage()), cause);
     }
 
 }
