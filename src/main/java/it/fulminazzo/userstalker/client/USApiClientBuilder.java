133c1814fcc31479ab6bd4f847a569f55364f36a
user <fulminazzo21@gmail.com>
2025-05-22 23:19:31 +0200

    Added getConfigurationValue method with fallback value in case of null

--- src/main/java/it/fulminazzo/userstalker/client/USApiClientBuilder.java
+++ src/main/java/it/fulminazzo/userstalker/client/USApiClientBuilder.java
@@ -9,6 +9,10 @@ import org.jetbrains.annotations.Nullable;
  * the given configuration.
  */
 final class USApiClientBuilder {
+    private static final String PATH = "userstalker-http-server";
+
+    private static final String MISSING_VALUE = "Invalid configuration detected: missing %s value.";
+
     private @Nullable FileConfiguration configuration;
 
     /**
@@ -33,5 +37,26 @@ final class USApiClientBuilder {
         return this;
     }
 
+    /**
+     * Gets a value from the configuration file.
+     *
+     * @param <T>    the type of the value
+     * @param path   the path
+     * @param clazz  the clas of the value
+     * @param orElse the value to return in case the configuration value is null
+     * @return the value
+     * @throws APIClientException thrown in case no value is provided
+     */
+    <T> @NotNull T getConfigurationValue(final @NotNull String path,
+                                         final @NotNull Class<T> clazz,
+                                         final @Nullable T orElse) throws APIClientException {
+        String actualPath = PATH + "." + path;
+        T value = getConfiguration().get(actualPath, clazz);
+        if (value == null)
+            if (orElse == null)
+                throw new APIClientException(String.format(MISSING_VALUE, actualPath));
+            else return orElse;
+        else return value;
+    }
 
 }
