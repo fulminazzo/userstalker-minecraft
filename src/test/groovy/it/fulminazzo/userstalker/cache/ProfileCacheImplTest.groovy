e43b998d6ff6f3aded90cf418f7bb823b3d89d15
user <fulminazzo21@gmail.com>
2025-05-22 12:35:44 +0200

    Added tests for lookupUserUUID method

--- src/test/groovy/it/fulminazzo/userstalker/cache/ProfileCacheImplTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/cache/ProfileCacheImplTest.groovy
@@ -7,15 +7,15 @@ import spock.lang.Specification
 class ProfileCacheImplTest extends Specification {
     private static final String ACTION = 'searching for userstalker repository'
 
-    private ProfileCacheImpl skinCache
+    private ProfileCacheImpl cache
 
     void setup() {
-        skinCache = new TestProfileCache()
+        cache = new TestProfileCache()
     }
 
     def 'test that lookupUserSkin of valid username returns expected value'() {
         when:
-        def skin = skinCache.lookupUserSkin('Notch')
+        def skin = cache.lookupUserSkin('Notch')
 
         then:
         skin.isPresent()
@@ -23,7 +23,7 @@ class ProfileCacheImplTest extends Specification {
 
     def 'test that lookupUserSkin of not existing player returns empty'() {
         when:
-        def skin = skinCache.lookupUserSkin('NotExistingAtAll')
+        def skin = cache.lookupUserSkin('NotExistingAtAll')
 
         then:
         !skin.isPresent()
@@ -47,12 +47,21 @@ class ProfileCacheImplTest extends Specification {
         createData(new Object(), ['name': 'textures', 'value': 'skin'], new Object()) || true
     }
 
+    def 'test that lookupUserUUID of valid username returns expected value'() {
+        when:
+        def uuid = cache.lookupUserUUID('Notch')
+
+        then:
+        uuid.isPresent()
+        uuid.get() == UUID.fromString('069a79f4-44e9-4726-a5be-fca90e38aaf5')
+    }
+
     def 'test that getJsonFromURL returns valid Json'() {
         given:
         def url = 'https://api.github.com/repos/fulminazzo/userstalker'
 
         when:
-        def json = skinCache.getJsonFromURL(url, ACTION).get()
+        def json = cache.getJsonFromURL(url, ACTION).get()
 
         then:
         json.get('name').asString == 'userstalker'
@@ -65,7 +74,7 @@ class ProfileCacheImplTest extends Specification {
         def url = 'https://api.github.com/repos/fulminazzo/notexisting'
 
         when:
-        def json = skinCache.getJsonFromURL(url, ACTION)
+        def json = cache.getJsonFromURL(url, ACTION)
 
         then:
         !json.isPresent()
@@ -73,7 +82,7 @@ class ProfileCacheImplTest extends Specification {
 
     def 'test that getJsonFromURL with invalid URL throws'() {
         when:
-        skinCache.getJsonFromURL('invalid', ACTION)
+        cache.getJsonFromURL('invalid', ACTION)
 
         then:
         def e = thrown(ProfileCacheException)
@@ -82,7 +91,7 @@ class ProfileCacheImplTest extends Specification {
 
     def 'test that getJsonFromURL with general error throws'() {
         when:
-        skinCache.getJsonFromURL('http://localhost', ACTION)
+        cache.getJsonFromURL('http://localhost', ACTION)
 
         then:
         def e = thrown(ProfileCacheException)
@@ -91,7 +100,7 @@ class ProfileCacheImplTest extends Specification {
 
     def 'test that getJsonFromURL with invalid code throws'() {
         when:
-        skinCache.getJsonFromURL('https://sessionserver.mojang.com/session/minecraft/profile/aa', ACTION)
+        cache.getJsonFromURL('https://sessionserver.mojang.com/session/minecraft/profile/aa', ACTION)
 
         then:
         def e = thrown(ProfileCacheException)
