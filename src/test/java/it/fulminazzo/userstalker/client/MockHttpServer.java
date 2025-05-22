462bc967c8f30fcf1e9a3d4ee3ee5e49a9219e0b
user <fulminazzo21@gmail.com>
2025-05-22 22:09:19 +0200

    Implemented /valid endpoint for testing purposes

--- src/test/java/it/fulminazzo/userstalker/client/MockHttpServer.java
+++ src/test/java/it/fulminazzo/userstalker/client/MockHttpServer.java
@@ -1,13 +1,17 @@
 package it.fulminazzo.userstalker.client;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class MockHttpServer implements HttpHandler {
+    private static final String API_PATH = "/api/v1/userlogins";
+
     private final HttpServer server;
 
     public MockHttpServer(int port) throws IOException {
@@ -27,18 +31,31 @@ public class MockHttpServer implements HttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         String method = httpExchange.getRequestMethod();
         String path = httpExchange.getRequestURI().getPath();
-        if (method.equals("GET")) handleGet(httpExchange, path);
-        else if (method.equals("POST")) handlePost(httpExchange, path);
-        else httpExchange.sendResponseHeaders(405, 0);
+        if (path.startsWith(API_PATH)) {
+            path = path.substring(API_PATH.length());
+            if (method.equals("GET")) handleGet(httpExchange, path);
+            else if (method.equals("POST")) handlePost(httpExchange, path);
+            else httpExchange.sendResponseHeaders(405, 0);
+        } else httpExchange.sendResponseHeaders(404, 0);
         httpExchange.close();
     }
 
     public void handleGet(HttpExchange httpExchange, String path) throws IOException {
-
+        if (path.equalsIgnoreCase("/valid")) sendResponse(httpExchange, "OK");
     }
 
     public void handlePost(HttpExchange httpExchange, String path) throws IOException {
 
+    }
+
+    private void sendResponse(HttpExchange httpExchange, Object response) throws IOException {
+        String rawResponse = new Gson().toJson(response);
+        httpExchange.getResponseHeaders().set("Content-Type", "application/json");
+        httpExchange.sendResponseHeaders(200, rawResponse.length());
+
+        try (OutputStream outputStream = httpExchange.getResponseBody()) {
+            outputStream.write(rawResponse.getBytes());
+        }
     }
 
 }
