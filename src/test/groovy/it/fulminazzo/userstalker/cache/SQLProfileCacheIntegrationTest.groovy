e0012eb770a93f191cd734a1ffa5afa7361929a9
user <fulminazzo21@gmail.com>
2025-05-22 13:58:41 +0200

    Created test suite for SQLProfileCache

---
+++ src/test/groovy/it/fulminazzo/userstalker/cache/SQLProfileCacheIntegrationTest.groovy
@@ -0,0 +1,29 @@
+package it.fulminazzo.userstalker.cache
+
+import spock.lang.Specification
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class SQLProfileCacheIntegrationTest extends Specification {
+    private static final String DB_URL = 'jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1'
+    private static final String DB_USER = 'sa'
+    private static final String DB_PASSWORD = ''
+
+    private Connection connection
+
+    private SQLProfileCache cache
+
+    void setup() {
+        Class.forName('org.h2.Driver')
+        connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)
+
+        cache = new SQLProfileCache(connection, 100 * 1000)
+    }
+
+    def 'test context loads'() {
+        expect:
+        true
+    }
+
+}
