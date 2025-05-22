848f79fb52b476323ce8844784652541c8145284
user <fulminazzo21@gmail.com>
2025-05-22 21:45:15 +0200

    Added empty query method

--- src/main/java/it/fulminazzo/userstalker/USApiClient.java
+++ src/main/java/it/fulminazzo/userstalker/USApiClient.java
@@ -2,6 +2,7 @@ package it.fulminazzo.userstalker;
 
 import it.fulminazzo.userstalker.domain.UserLogin;
 import it.fulminazzo.userstalker.domain.UserLoginCount;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,7 +12,12 @@ import java.util.List;
  * This class directly interfaces with the REST API
  * of UserStalker to retrieve and share data.
  */
+@RequiredArgsConstructor
 public final class USApiClient {
+    private static final String API_PATH = "/api/v1/userlogins";
+
+    private final @NotNull String address;
+    private final int port;
 
     public void notifyUserLogin(final @NotNull Player player) {
 
@@ -34,6 +40,12 @@ public final class USApiClient {
     }
 
     public @NotNull List<UserLogin> getUserLogins(final @NotNull String username) {
+        return null;
+    }
+
+    @NotNull <T> T query(final @NotNull String method,
+                         final @NotNull String path,
+                         final @NotNull Class<T> convertClass) {
         return null;
     }
 
