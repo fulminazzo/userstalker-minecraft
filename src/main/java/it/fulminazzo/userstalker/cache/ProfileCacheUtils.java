c0d606beb68805607d74788c74d39b64b33692a6
user <fulminazzo21@gmail.com>
2025-05-22 13:43:58 +0200

    Created ProfileCacheUtils class

---
+++ src/main/java/it/fulminazzo/userstalker/cache/ProfileCacheUtils.java
@@ -0,0 +1,12 @@
+package it.fulminazzo.userstalker.cache;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * A collection of utilities used in this package.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class ProfileCacheUtils {
+    
+}
