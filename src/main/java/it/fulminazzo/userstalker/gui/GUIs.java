4f55b77e0a1b96dcc21996f4527879831db067b8
user <fulminazzo21@gmail.com>
2025-05-23 23:37:29 +0200

    Extracted setCommonGUISettings function

--- src/main/java/it/fulminazzo/userstalker/gui/GUIs.java
+++ src/main/java/it/fulminazzo/userstalker/gui/GUIs.java
@@ -3,10 +3,12 @@ package it.fulminazzo.userstalker.gui;
 import it.fulminazzo.userstalker.domain.UserLogin;
 import it.fulminazzo.yagl.contents.ItemGUIContent;
 import it.fulminazzo.yagl.guis.DataGUI;
+import it.fulminazzo.yagl.guis.PageableGUI;
 import it.fulminazzo.yagl.items.Item;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.function.Function;
@@ -17,17 +19,21 @@ import java.util.function.Function;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GUIs {
 
-    public static final Function<Collection<UserLogin>, DataGUI<UserLogin>> USER_LOGINS_GUI_PROVIDER = data -> {
-        DataGUI<UserLogin> dataGUI = DataGUI.newGUI(54, u -> ItemGUIContent.newInstance(Material.BOOK.name())
-                        .setDisplayName(String.format("&fIp: &c%s", u.getIp()))
-                        .setLore(String.format("&fLogin date: &a%s %s", u.getLoginDate().toLocalTime(), u.getLoginDate().toLocalDate())),
-                data);
-        return dataGUI.setAllSides(Item.newItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE.name()).setDisplayName(" "))
+    public static final Function<Collection<UserLogin>, DataGUI<UserLogin>> USER_LOGINS_GUI_PROVIDER = data ->
+            setCommonGUISettings(DataGUI.newGUI(54, u -> ItemGUIContent.newInstance(Material.BOOK.name())
+                            .setDisplayName(String.format("&fIp: &c%s", u.getIp()))
+                            .setLore(String.format("&fLogin date: &a%s %s",
+                                    u.getLoginDate().toLocalTime(), u.getLoginDate().toLocalDate())),
+                    data));
+
+    @SuppressWarnings("unchecked")
+    private static <G extends PageableGUI> @NotNull G setCommonGUISettings(final @NotNull G gui) {
+        return (G) gui.setAllSides(Item.newItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE.name()).setDisplayName(" "))
                 .setPreviousPage(47, Item.newItem(Material.PAPER.name()).setDisplayName("&ePrevious page"))
                 .setNextPage(51, Item.newItem(Material.PAPER.name()).setDisplayName("&eNext page"))
                 .setContents(49, Item.newItem(Material.OBSIDIAN.name())
                         .setDisplayName("&fCurrent page: &e<page>&8/&a<pages>")
                 );
-    };
+    }
 
 }
