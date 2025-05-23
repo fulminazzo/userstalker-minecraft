655468552423f201a4222278e773de8b4a9a45ba
user <fulminazzo21@gmail.com>
2025-05-23 13:56:23 +0200

    Added tests for notifyUserLogin method and extracted newClient function

--- src/test/groovy/it/fulminazzo/userstalker/USAsyncApiClientTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/USAsyncApiClientTest.groovy
@@ -14,27 +14,40 @@ class USAsyncApiClientTest extends Specification {
     private USAsyncApiClient client
 
     void setup() {
-        def scheduler = Mock(BukkitScheduler)
-        scheduler.runTaskAsynchronously(_, _ as Runnable) >> { args ->
-            Runnable runnable = args[1] as Runnable
-            runnable.run()
-        }
-
-        def logger = Logger.getLogger(getClass().simpleName)
-
-        def configuration = new MockFileConfiguration([
-                'userstalker-http-server': [
-                        'address': 'http://localhost'
-                ]
-        ])
-
-        client = new USAsyncApiClient(null, logger, scheduler, configuration)
+        client = newClient('http://localhost')
 
         server.start()
     }
 
     void cleanup() {
         server.stop()
+    }
+
+    def 'test notifyUserLogin does not throw'() {
+        given:
+        def username = 'Fulminazzo'
+        def ip = new InetSocketAddress('localhost', 8080)
+
+        when:
+        client.notifyUserLogin(username, ip)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'test notifyUserLogin does not throw on APIClientException'() {
+        given:
+        def username = 'Fulminazzo'
+        def ip = new InetSocketAddress('localhost', 8080)
+
+        and:
+        def client = newClient('invalid')
+
+        when:
+        client.notifyUserLogin(username, ip)
+
+        then:
+        noExceptionThrown()
     }
 
     def 'test context loads'() {
@@ -46,6 +59,24 @@ class USAsyncApiClientTest extends Specification {
 
         then:
         test
+    }
+
+    private USAsyncApiClient newClient(String host) {
+        def scheduler = Mock(BukkitScheduler)
+        scheduler.runTaskAsynchronously(_, _ as Runnable) >> { args ->
+            Runnable runnable = args[1] as Runnable
+            runnable.run()
+        }
+
+        def logger = Logger.getLogger(getClass().simpleName)
+
+        def configuration = new MockFileConfiguration([
+                'userstalker-http-server': [
+                        'address': host
+                ]
+        ])
+
+        return new USAsyncApiClient(null, logger, scheduler, configuration)
     }
 
 }
