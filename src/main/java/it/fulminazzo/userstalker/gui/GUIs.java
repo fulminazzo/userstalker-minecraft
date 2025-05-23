010b0f9eb22825d1dbc332a919a68776528d6621
user <fulminazzo21@gmail.com>
2025-05-23 22:44:44 +0200

    Created GUIs collection class

---
+++ src/main/java/it/fulminazzo/userstalker/gui/GUIs.java
@@ -0,0 +1,11 @@
+package it.fulminazzo.userstalker.gui;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * A collection of default GUIs.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class GUIs {
+}
