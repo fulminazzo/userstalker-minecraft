e9cee5f3eccc323feb69fe3ed6e3ed7c8cea40c0
user <fulminazzo21@gmail.com>
2025-05-25 21:54:53 +0200

    Updated tests with null cases

--- src/test/groovy/it/fulminazzo/userstalker/utils/GsonUtilsTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/utils/GsonUtilsTest.groovy
@@ -6,32 +6,30 @@ import java.time.LocalDateTime
 
 class GsonUtilsTest extends Specification {
 
-    def 'test that gson can convert LocalDateTime to Json'() {
-        given:
-        def localDateTime = LocalDateTime.of(2025, 5, 25, 21, 48)
-
-        and:
-        def expected = '"2025-05-25 21:48:00"'
-
+    def 'test that gson can convert #localDateTime to #expected'() {
         when:
-        def actual = GsonUtils.gson.toJson(localDateTime)
+        def actual = GsonUtils.gson.toJson(localDateTime, LocalDateTime)
 
         then:
         actual == expected
+
+        where:
+        localDateTime                         || expected
+        LocalDateTime.of(2025, 5, 25, 21, 48) || '"2025-05-25 21:48:00"'
+        null                                  || 'null'
     }
 
-    def 'test that gson can convert Json to LocalDateTime'() {
-        given:
-        def json = '"2025-05-25 21:48:00"'
-
-        and:
-        def expected = LocalDateTime.of(2025, 5, 25, 21, 48)
-
+    def 'test that gson can convert #json to #expected'() {
         when:
         def actual = GsonUtils.gson.fromJson(json, LocalDateTime)
 
         then:
         actual == expected
+
+        where:
+        json                    || expected
+        '"2025-05-25 21:48:00"' || LocalDateTime.of(2025, 5, 25, 21, 48)
+        'null'                  || null
     }
 
 }
