a8ecc522a01f8452d1f4b05d63818d24d648b5d1
user <fulminazzo21@gmail.com>
2025-05-25 19:50:43 +0200

    Added userLoginsGUI field and load logic

--- src/main/java/it/fulminazzo/userstalker/gui/USGUIManager.java
+++ src/main/java/it/fulminazzo/userstalker/gui/USGUIManager.java
@@ -1,6 +1,9 @@
 package it.fulminazzo.userstalker.gui;
 
 import it.fulminazzo.userstalker.client.USAsyncApiClient;
+import it.fulminazzo.userstalker.domain.UserLogin;
+import it.fulminazzo.yagl.guis.DataGUI;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import it.fulminazzo.yamlparser.utils.FileUtils;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -17,6 +20,8 @@ public final class USGUIManager {
     private final @NotNull Logger logger;
     private final @NotNull USAsyncApiClient client;
 
+    private DataGUI<UserLogin> userLoginsGUI;
+
     // getTopUserLoginsAndThen
     // getMonthlyUserLoginsAndThen
     // getNewestUserLoginsAndThen
@@ -28,6 +33,7 @@ public final class USGUIManager {
      * @param pluginDirectory the plugin directory
      * @return true if everything was successful
      */
+    @SuppressWarnings("unchecked")
     public boolean setup(final @NotNull File pluginDirectory) {
         File file = new File(pluginDirectory, "guis.yml");
 
@@ -41,9 +47,23 @@ public final class USGUIManager {
                         e.getMessage()));
                 return false;
             }
+            FileConfiguration config = FileConfiguration.newConfiguration(file);
             // Store GUIs in file and load in memory
+
+            userLoginsGUI = GUIs.defaultUserLogins();
+            config.set("user-logins", userLoginsGUI);
+
+            config.save();
         } else {
+            FileConfiguration config = FileConfiguration.newConfiguration(file);
             // Load GUIs in memory, warn for errors
+
+            if (config.contains("user-logins"))
+                userLoginsGUI = config.get("user-logins", DataGUI.class);
+            else {
+                logger.warning("Could not find user-logins in guis.yml. Using default GUI");
+                userLoginsGUI = GUIs.defaultUserLogins();
+            }
         }
         return true;
     }
