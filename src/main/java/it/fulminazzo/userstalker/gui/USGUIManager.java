e983485df5edc5e0e054a43681fae43632d46a66
user <fulminazzo21@gmail.com>
2025-05-25 20:56:25 +0200

    Added other GUIs as fields

--- src/main/java/it/fulminazzo/userstalker/gui/USGUIManager.java
+++ src/main/java/it/fulminazzo/userstalker/gui/USGUIManager.java
@@ -2,6 +2,7 @@ package it.fulminazzo.userstalker.gui;
 
 import it.fulminazzo.userstalker.client.USAsyncApiClient;
 import it.fulminazzo.userstalker.domain.UserLogin;
+import it.fulminazzo.userstalker.domain.UserLoginCount;
 import it.fulminazzo.yagl.guis.DataGUI;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import it.fulminazzo.yamlparser.utils.FileUtils;
@@ -20,6 +21,9 @@ public final class USGUIManager {
     private final @NotNull Logger logger;
     private final @NotNull USAsyncApiClient client;
 
+    private DataGUI<UserLoginCount> topUsersLoginsGUI;
+    private DataGUI<UserLoginCount> monthlyUsersLoginsGUI;
+    private DataGUI<UserLogin> newestUsersLoginsGUI;
     private DataGUI<UserLogin> userLoginsGUI;
 
     // getTopUserLoginsAndThen
@@ -33,7 +37,6 @@ public final class USGUIManager {
      * @param pluginDirectory the plugin directory
      * @return true if everything was successful
      */
-    @SuppressWarnings("unchecked")
     public boolean setup(final @NotNull File pluginDirectory) {
         File file = new File(pluginDirectory, "guis.yml");
 
@@ -50,6 +53,12 @@ public final class USGUIManager {
             FileConfiguration config = FileConfiguration.newConfiguration(file);
             // Store GUIs in file and load in memory
 
+            topUsersLoginsGUI = setAndGet(config, "top-users-logins", GUIs.defaultTopUsersLogins());
+
+            monthlyUsersLoginsGUI = setAndGet(config, "monthly-users-logins", GUIs.defaultMonthlyUsersLogins());
+
+            newestUsersLoginsGUI = setAndGet(config, "newest-users-logins", GUIs.defaultNewestUsersLogins());
+
             userLoginsGUI = setAndGet(config, "user-logins", GUIs.defaultUserLogins());
 
             config.save();
@@ -57,7 +66,14 @@ public final class USGUIManager {
             FileConfiguration config = FileConfiguration.newConfiguration(file);
             // Load GUIs in memory, warn for errors
 
-            userLoginsGUI = getGUI(config, "user-logins");
+            topUsersLoginsGUI = getGUI(config, "top-users-logins", GUIs.defaultTopUsersLogins());
+
+            monthlyUsersLoginsGUI = getGUI(config, "monthly-users-logins", GUIs.defaultMonthlyUsersLogins());
+
+            newestUsersLoginsGUI = getGUI(config, "newest-users-logins", GUIs.defaultNewestUsersLogins());
+
+            userLoginsGUI = getGUI(config, "user-logins", GUIs.defaultUserLogins());
+
         }
         return true;
     }
@@ -71,12 +87,13 @@ public final class USGUIManager {
 
     @SuppressWarnings("unchecked")
     private <T> DataGUI<T> getGUI(final @NotNull FileConfiguration config,
-                                  final @NotNull String path) {
+                                  final @NotNull String path,
+                                  final @NotNull DataGUI<T> defaultGUI) {
         if (config.contains(path))
             return config.get(path, DataGUI.class);
         else {
             logger.warning(String.format("Could not find %s in guis.yml. Using default GUI", path));
-            return (DataGUI<T>) GUIs.defaultUserLogins();
+            return defaultGUI;
         }
     }
 
