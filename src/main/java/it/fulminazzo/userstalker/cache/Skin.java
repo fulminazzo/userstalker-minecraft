30343ec2e274dbe1816594c5cb8064cd283661f2
user <fulminazzo21@gmail.com>
2025-05-25 16:50:16 +0200

    Created Skin class

---
+++ src/main/java/it/fulminazzo/userstalker/cache/Skin.java
@@ -0,0 +1,23 @@
+package it.fulminazzo.userstalker.cache;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public final class Skin {
+
+    private UUID uuid;
+
+    private String username;
+
+    private String skin;
+
+    private String signature;
+
+}
