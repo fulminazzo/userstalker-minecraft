5bc0003ab996e167ebc7d0fdb31261c3f43857c5
user <fulminazzo21@gmail.com>
2025-05-26 17:05:53 +0200

    Prepared UserStalker tests with mock object

--- src/test/groovy/it/fulminazzo/userstalker/UserStalkerTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/UserStalkerTest.groovy
@@ -1,8 +1,25 @@
 package it.fulminazzo.userstalker
 
+import it.fulminazzo.fulmicommands.messages.DefaultFulmiMessages
 import spock.lang.Specification
 
+import java.util.logging.Logger
+
 class UserStalkerTest extends Specification {
+
+    private static final File PLUGIN_DIRECTORY = new File('build/resources/test')
+
+    private UserStalker plugin
+
+    void setup() {
+        plugin = Mock(UserStalker)
+        plugin.setupConfiguration() >> { callRealMethod() }
+        plugin.configuration >> { callRealMethod() }
+        plugin.setupMessages(_ as DefaultFulmiMessages[]) >> { callRealMethod() }
+        plugin.messages >> { callRealMethod() }
+        plugin.logger >> Logger.getLogger(getClass().simpleName)
+        plugin.pluginDirectory >> PLUGIN_DIRECTORY
+    }
 
     def 'test spock tests work'() {
         expect:
