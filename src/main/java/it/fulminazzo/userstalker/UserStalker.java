df2645b777f489cd13ad746b71c3cff447627b6b
user <fulminazzo21@gmail.com>
2025-05-26 15:41:45 +0200

    Updated UserStalker with FulmiMessagesPlugin and implemented methods

--- src/main/java/it/fulminazzo/userstalker/UserStalker.java
+++ src/main/java/it/fulminazzo/userstalker/UserStalker.java
@@ -1,8 +1,18 @@
 package it.fulminazzo.userstalker;
 
+import it.fulminazzo.fulmicommands.FulmiException;
+import it.fulminazzo.fulmicommands.FulmiMessagesPlugin;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public final class UserStalker extends JavaPlugin {
+import java.io.File;
+
+public final class UserStalker extends JavaPlugin implements FulmiMessagesPlugin {
+
+    private @Nullable FileConfiguration configuration;
+    private @Nullable FileConfiguration messages;
 
     @Override
     public void onEnable() {
@@ -12,6 +22,23 @@ public final class UserStalker extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public @NotNull FileConfiguration getConfiguration() {
+        if (configuration == null) throw FulmiException.configurationNotLoaded("config.yml");
+        return configuration;
+    }
+
+    @Override
+    public @NotNull FileConfiguration getMessages() {
+        if (messages == null) throw FulmiException.configurationNotLoaded("messages.yml");
+        return messages;
+    }
+
+    @Override
+    public @NotNull File getPluginDirectory() {
+        return getDataFolder();
     }
 
 }
