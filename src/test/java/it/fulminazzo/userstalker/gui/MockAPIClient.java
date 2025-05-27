5848ce0640a013d73afaef80809f67dc4cc280e7
user <fulminazzo21@gmail.com>
2025-05-27 18:25:14 +0200

    Overridden several methods implementations and added switches for testing

--- src/test/java/it/fulminazzo/userstalker/gui/MockAPIClient.java
+++ src/test/java/it/fulminazzo/userstalker/gui/MockAPIClient.java
@@ -4,6 +4,7 @@ import it.fulminazzo.userstalker.MockFileConfiguration;
 import it.fulminazzo.userstalker.client.APIClientException;
 import it.fulminazzo.userstalker.client.USAsyncApiClient;
 import it.fulminazzo.userstalker.domain.UserLogin;
+import it.fulminazzo.userstalker.domain.UserLoginCount;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,9 +17,39 @@ import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 public class MockAPIClient extends USAsyncApiClient {
+    private boolean topUsersEnabled;
+    private boolean monthlyUsersEnabled;
+    private boolean newestUsersEnabled;
 
     public MockAPIClient() throws APIClientException {
         super(Logger.getLogger("MockAPIClient"), generateMockConfiguration());
+    }
+
+    @Override
+    public void getTopUserLoginsAndThen(@NotNull Consumer<List<UserLoginCount>> function, @NotNull Runnable orElse) {
+        if (topUsersEnabled) function.accept(Arrays.asList(
+                UserLoginCount.builder().username("Alex").loginCount(20).build(),
+                UserLoginCount.builder().username("Fulminazzo").loginCount(10).build()
+        ));
+        else orElse.run();
+    }
+
+    @Override
+    public void getMonthlyUserLoginsAndThen(@NotNull Consumer<List<UserLoginCount>> function, @NotNull Runnable orElse) {
+        if (monthlyUsersEnabled) function.accept(Arrays.asList(
+                UserLoginCount.builder().username("Alex").loginCount(20).build(),
+                UserLoginCount.builder().username("Fulminazzo").loginCount(10).build()
+        ));
+        else orElse.run();
+    }
+
+    @Override
+    public void getNewestUserLoginsAndThen(@NotNull Consumer<List<UserLogin>> function, @NotNull Runnable orElse) {
+        if (newestUsersEnabled) function.accept(Arrays.asList(
+                UserLogin.builder().username("Alex").ip("127.0.0.1").loginDate(LocalDateTime.now()).build(),
+                UserLogin.builder().username("Fulminazzo").ip("127.0.0.2").loginDate(LocalDateTime.now()).build()
+        ));
+        else orElse.run();
     }
 
     @Override
@@ -35,6 +66,18 @@ public class MockAPIClient extends USAsyncApiClient {
     @Override
     protected void runAsync(@NotNull Runnable runnable) {
 
+    }
+
+    public void disableTop() {
+        topUsersEnabled = false;
+    }
+
+    public void disableMonthly() {
+        monthlyUsersEnabled = false;
+    }
+
+    public void disableNewest() {
+        newestUsersEnabled = false;
     }
 
     private static @NotNull FileConfiguration generateMockConfiguration() {
