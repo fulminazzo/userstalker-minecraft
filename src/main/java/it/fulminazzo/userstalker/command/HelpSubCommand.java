1d9ab4f84f680c2f82d238adcce21097cb99789c
user <fulminazzo21@gmail.com>
2025-05-27 22:30:07 +0200

    Optimized code

--- src/main/java/it/fulminazzo/userstalker/command/HelpSubCommand.java
+++ src/main/java/it/fulminazzo/userstalker/command/HelpSubCommand.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class HelpSubCommand extends USSubCommand {
     private final @NotNull USCommand command;
@@ -20,41 +21,42 @@ final class HelpSubCommand extends USSubCommand {
 
     @Override
     public void execute(@NotNull CommandSender sender, @NotNull String[] args) {
+        List<USSubCommand> commands = getExecutableCommands(sender).collect(Collectors.toList());
         if (args.length == 1) {
-            List<USSubCommand> commands = command.getSubCommands().stream()
-                    .filter(c -> sender.hasPermission(c.getPermission()))
-                    .filter(c -> c.getAliases().stream()
-                            .anyMatch(a -> a.toLowerCase().startsWith(args[0].toLowerCase()))
-                    )
-                    .collect(Collectors.toList());
-            if (commands.isEmpty())
+            String argument = args[0];
+            commands.removeIf(c -> c.getAliases().stream()
+                    .anyMatch(a -> a.toLowerCase().startsWith(argument.toLowerCase())));
+            if (commands.isEmpty()) {
                 sender.sendMessage(Messages.SUBCOMMAND_NOT_FOUND.getMessage()
-                        .replace("<subcommand>", args[0]));
-            else commands.forEach(c -> sender.sendMessage(Messages.HELP_DESCRIPTION.getMessage()
-                    .replace("<name>", c.getAliases().get(0))
-                    .replace("<aliases>", String.join(", ", c.getAliases()))
-                    .replace("<description>", c.getDescription())
-            ));
-        } else command.getSubCommands().stream()
-                .filter(c -> sender.hasPermission(c.getPermission()))
-                .forEach(c -> sender.sendMessage(Messages.HELP_DESCRIPTION.getMessage()
-                        .replace("<name>", c.getAliases().get(0))
-                        .replace("<aliases>", String.join(", ", c.getAliases()))
-                        .replace("<description>", c.getDescription())
-                ));
+                        .replace("<subcommand>", argument));
+                return;
+            }
+        } else if (commands.isEmpty()) {
+            sender.sendMessage(Messages.SUBCOMMANDS_NO_PERMISSIONS.getMessage());
+            return;
+        }
+        commands.forEach(c -> sender.sendMessage(Messages.HELP_DESCRIPTION.getMessage()
+                .replace("<name>", c.getAliases().get(0))
+                .replace("<aliases>", String.join(", ", c.getAliases()))
+                .replace("<description>", c.getDescription())
+        ));
     }
 
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String[] args) {
         List<String> list = new ArrayList<>();
         if (args.length == 1)
-            list.addAll(command.getSubCommands().stream()
-                    .filter(c -> sender.hasPermission(c.getPermission()))
+            list.addAll(getExecutableCommands(sender)
                     .map(USSubCommand::getAliases)
                     .flatMap(Collection::stream)
                     .collect(Collectors.toList())
             );
         return list;
+    }
+
+    private @NotNull Stream<USSubCommand> getExecutableCommands(@NotNull CommandSender sender) {
+        return command.getSubCommands().stream()
+                .filter(c -> sender.hasPermission(c.getPermission()));
     }
 
 }
