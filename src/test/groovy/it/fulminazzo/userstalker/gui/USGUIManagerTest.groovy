654c54e7322265a7b49a7da31e294ef14df51d08
user <fulminazzo21@gmail.com>
2025-05-27 14:45:11 +0200

    Updated tests for USGUIManager

--- src/test/groovy/it/fulminazzo/userstalker/gui/USGUIManagerTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/gui/USGUIManagerTest.groovy
@@ -1,7 +1,27 @@
 package it.fulminazzo.userstalker.gui
 
+import it.fulminazzo.userstalker.client.USAsyncApiClient
+import it.fulminazzo.yagl.parsers.GUIYAGLParser
 import spock.lang.Specification
 
 class USGUIManagerTest extends Specification {
+
+    private static final File PLUGIN_DIRECTORY = new File('build/resources/test/usguimanager')
+
+    private static USGUIManager manager
+
+    void setupSpec() {
+        GUIYAGLParser.addAllParsers()
+
+        manager = USGUIManager.builder()
+                .apiClient(Mock(USAsyncApiClient))
+                .pluginDirectory(PLUGIN_DIRECTORY)
+                .build()
+    }
+
+    def 'test manager does load'() {
+        expect:
+        true
+    }
 
 }
