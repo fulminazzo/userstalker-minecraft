430382cf679a673ba8a6adade1eac05d80afb261
user <fulminazzo21@gmail.com>
2025-05-27 12:53:44 +0200

    Copied setup method from USGUIManager to USGUIManagerBuilder build method

--- src/main/java/it/fulminazzo/userstalker/gui/USGUIManagerBuilder.java
+++ src/main/java/it/fulminazzo/userstalker/gui/USGUIManagerBuilder.java
@@ -1,9 +1,17 @@
 package it.fulminazzo.userstalker.gui;
 
 import it.fulminazzo.fulmicommands.configuration.ConfigurationException;
+import it.fulminazzo.fulmicommands.configuration.ConfigurationType;
+import it.fulminazzo.fulmicommands.configuration.Configurator;
 import it.fulminazzo.userstalker.builder.LoggedBuilder;
 import it.fulminazzo.userstalker.cache.ProfileCache;
 import it.fulminazzo.userstalker.client.USAsyncApiClient;
+import it.fulminazzo.userstalker.domain.UserLogin;
+import it.fulminazzo.userstalker.domain.UserLoginCount;
+import it.fulminazzo.yagl.contents.GUIContent;
+import it.fulminazzo.yagl.contents.ItemGUIContent;
+import it.fulminazzo.yagl.guis.DataGUI;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -11,11 +19,15 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 
+import static it.fulminazzo.userstalker.gui.GUIs.*;
+
 /**
  * A builder to create {@link USGUIManager} instances.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class USGUIManagerBuilder extends LoggedBuilder<USGUIManager, USGUIManagerBuilder, ConfigurationException> {
+    private static final int DEFAULT_BACK_GUI_CONTENT_SLOT_OFFSET = -9;
+
     private @Nullable File pluginDirectory;
     private @Nullable USAsyncApiClient apiClient;
 
@@ -23,6 +35,52 @@ public class USGUIManagerBuilder extends LoggedBuilder<USGUIManager, USGUIManage
 
     @Override
     public @NotNull USGUIManager build() throws ConfigurationException {
+        final @NotNull File pluginDirectory = getPluginDirectory();
+
+        FileConfiguration config = Configurator.newBuilder()
+                .pluginDirectory(pluginDirectory)
+                .name("guis")
+                .type(ConfigurationType.YAML)
+                .onCreated(c -> {
+                    getLogger().ifPresent(logger ->
+                            logger.info(String.format("Created new configuration file: %s/guis.yml", pluginDirectory.getPath())));
+
+                    c.set("guis.top-users-logins", defaultTopUsersLogins());
+                    c.set("items.top-users-logins", defaultUserLoginCountItem());
+
+                    c.set("guis.monthly-users-logins", defaultMonthlyUsersLogins());
+                    c.set("items.monthly-users-logins", defaultUserLoginCountItem());
+
+                    c.set("guis.newest-users-logins", defaultNewestUsersLogins());
+                    c.set("items.newest-users-logins", defaultNamedUserLoginItem());
+
+                    c.set("guis.user-logins", defaultUserLogins());
+                    c.set("items.user-logins", defaultUserLoginItem());
+
+                    c.set("items.back", defaultBackItem());
+
+                    c.set("misc.back-offset", DEFAULT_BACK_GUI_CONTENT_SLOT_OFFSET);
+
+                    c.save();
+                })
+                .build();
+
+        DataGUI<UserLoginCount> topUsersLoginsGUI = getGUI(config, "guis.top-users-logins", defaultTopUsersLogins());
+        GUIContent topUsersLoginsGUIContent = getContent(config, "items.top-users-logins", defaultUserLoginCountItem());
+
+        DataGUI<UserLoginCount> monthlyUsersLoginsGUI = getGUI(config, "guis.monthly-users-logins", defaultMonthlyUsersLogins());
+        GUIContent monthlyUsersLoginsGUIContent = getContent(config, "items.monthly-users-logins", defaultUserLoginCountItem());
+
+        DataGUI<UserLogin> newestUsersLoginsGUI = getGUI(config, "guis.newest-users-logins", defaultNewestUsersLogins());
+        GUIContent newestUsersLoginsGUIContent = getContent(config, "items.newest-users-logins", defaultNamedUserLoginItem());
+
+        DataGUI<UserLogin> userLoginsGUI = getGUI(config, "guis.user-logins", defaultUserLogins());
+        GUIContent userLoginsGUIContent = getContent(config, "items.user-logins", defaultUserLoginItem());
+
+        GUIContent backGUIContent = config.get("items.back", ItemGUIContent.class);
+
+        int backGUIContentSlotOffset = getBackContentOffset(config);
+
         return null;
     }
 
@@ -84,6 +142,43 @@ public class USGUIManagerBuilder extends LoggedBuilder<USGUIManager, USGUIManage
     @Override
     protected @NotNull ConfigurationException newException(@NotNull String message) {
         return new ConfigurationException(message);
+    }
+
+    private <T> DataGUI<T> getGUI(final @NotNull FileConfiguration config,
+                                  final @NotNull String path,
+                                  final @NotNull DataGUI<T> defaultGUI) {
+        if (config.contains(path)) {
+            DataGUI<T> gui = config.get(path, DataGUI.class);
+            if (gui != null) return gui;
+        }
+        getLogger().ifPresent(logger -> logger.warning(String.format("Could not find gui \"%s\" in guis.yml. Using default GUI", path)));
+        return defaultGUI;
+    }
+
+    private GUIContent getContent(final @NotNull FileConfiguration config,
+                                  final @NotNull String path,
+                                  final @NotNull GUIContent defaultContent) {
+        if (config.contains(path)) {
+            ItemGUIContent item = config.get(path, ItemGUIContent.class);
+            if (item != null) return item;
+        }
+        getLogger().ifPresent(logger -> logger.warning(String.format("Could not find item \"%s\" in guis.yml. Using default item", path)));
+        return defaultContent;
+    }
+
+    private int getBackContentOffset(final @NotNull FileConfiguration config) {
+        String path = "misc.back-offset";
+        Integer slot = config.getInteger(path);
+        if (slot == null) {
+            getLogger().ifPresent(logger -> logger.warning(String.format("Could not find \"%s\" in guis.yml. Using default offset", path)));
+            return DEFAULT_BACK_GUI_CONTENT_SLOT_OFFSET;
+        }
+        if (slot > -1) {
+            getLogger().ifPresent(logger ->
+                    logger.warning("Invalid back-offset specified in guis.yml. Required a negative number lower than or equal to -1. Using default offset"));
+            return DEFAULT_BACK_GUI_CONTENT_SLOT_OFFSET;
+        }
+        return slot;
     }
 
 }
