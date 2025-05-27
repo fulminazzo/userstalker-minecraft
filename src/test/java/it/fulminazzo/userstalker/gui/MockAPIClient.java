afa096bf8e7855c1e271c766475d70bc96583879
user <fulminazzo21@gmail.com>
2025-05-27 17:51:26 +0200

    Added data to MockConfiguration

--- src/test/java/it/fulminazzo/userstalker/gui/MockAPIClient.java
+++ src/test/java/it/fulminazzo/userstalker/gui/MockAPIClient.java
@@ -4,19 +4,21 @@ import it.fulminazzo.userstalker.MockFileConfiguration;
 import it.fulminazzo.userstalker.client.APIClientException;
 import it.fulminazzo.userstalker.client.USAsyncApiClient;
 import it.fulminazzo.userstalker.domain.UserLogin;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 public class MockAPIClient extends USAsyncApiClient {
 
     public MockAPIClient() throws APIClientException {
-        super(Logger.getLogger("MockAPIClient"), new MockFileConfiguration(new HashMap<>()));
+        super(Logger.getLogger("MockAPIClient"), generateMockConfiguration());
     }
 
     @Override
@@ -33,6 +35,14 @@ public class MockAPIClient extends USAsyncApiClient {
     @Override
     protected void runAsync(@NotNull Runnable runnable) {
 
+    }
+
+    private static @NotNull FileConfiguration generateMockConfiguration() {
+        Map<String, Object> data = new HashMap<>();
+        Map<String, Object> serverData = new HashMap<>();
+        serverData.put("address", "localhost");
+        data.put("userstalker-http-server", serverData);
+        return new MockFileConfiguration(data);
     }
 
 }
