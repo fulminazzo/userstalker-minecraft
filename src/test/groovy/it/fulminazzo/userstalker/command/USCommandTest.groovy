e11bcfd130ab6f2e45a1fd4743ea888fc0b004dc
user <fulminazzo21@gmail.com>
2025-05-27 23:42:55 +0200

    Added tests for onCommand method

--- src/test/groovy/it/fulminazzo/userstalker/command/USCommandTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/command/USCommandTest.groovy
@@ -1,5 +1,6 @@
 package it.fulminazzo.userstalker.command
 
+import it.fulminazzo.userstalker.Messages
 import it.fulminazzo.userstalker.MockFileConfiguration
 import it.fulminazzo.userstalker.UserStalker
 import org.bukkit.command.CommandSender
@@ -17,11 +18,46 @@ class USCommandTest extends Specification {
         def plugin = Mock(UserStalker)
         plugin.messages >> new MockFileConfiguration([:])
         plugin.logger >> Logger.getLogger(getClass().simpleName)
+        plugin.name >> 'UserStalker'
 
         command = new USCommand(plugin)
 
         sender = Mock(CommandSender)
-        sender.hasPermission(_ as String) >> true
+        sender.hasPermission(_ as String) >> { args ->
+            String permission = args[0]
+            return !permission.equals('userstalker.command.reload')
+        }
+
+        UserStalker.instance = plugin
+    }
+
+    def 'test that onCommand with args #args sends #expected messages'() {
+        given:
+        def sentMessages = []
+
+        and:
+        sender.sendMessage(_ as String) >> { a -> sentMessages.add(a[0]) }
+
+        when:
+        command.onCommand(
+                sender,
+                null,
+                '',
+                args.toArray(new String[0])
+        )
+
+        then:
+        sentMessages.containsAll(expected
+                .collect { it.message }
+                .collect { it.replace('<subcommand>', 'invalid') }
+        )
+
+        where:
+        args        || expected
+        ['invalid'] || [Messages.SUBCOMMAND_NOT_FOUND]
+        ['reload']  || [Messages.NOT_ENOUGH_PERMISSIONS]
+        []          || [Messages.NOT_ENOUGH_ARGUMENTS]
+        ['help']    || [Messages.HELP_DESCRIPTION]
     }
 
     def 'test that onTabComplete with args #args returns #expected'() {
