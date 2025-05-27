4f33167781a44d9e680821eaa1b0a0b2187036dc
user <fulminazzo21@gmail.com>
2025-05-27 22:21:44 +0200

    Created USCommand

---
+++ src/main/java/it/fulminazzo/userstalker/command/USCommand.java
@@ -0,0 +1,46 @@
+package it.fulminazzo.userstalker.command;
+
+import it.fulminazzo.userstalker.UserStalker;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The main command of UserStalker.
+ */
+public final class USCommand implements TabExecutor {
+    private final @NotNull UserStalker plugin;
+    private final @NotNull List<USSubCommand> subCommands;
+
+    /**
+     * Instantiates a new Us command.
+     *
+     * @param plugin the plugin
+     */
+    public USCommand(final @NotNull UserStalker plugin) {
+        this.plugin = plugin;
+        this.subCommands = Arrays.asList();
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender,
+                             @NotNull Command command,
+                             @NotNull String label,
+                             @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender,
+                                               @NotNull Command command,
+                                               @NotNull String alias,
+                                               @NotNull String[] args) {
+        return Collections.emptyList();
+    }
+
+}
