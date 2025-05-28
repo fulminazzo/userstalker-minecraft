56f5e360a80c7a11aec5576960f4fa815fb7b4bd
user <fulminazzo21@gmail.com>
2025-05-28 10:29:54 +0200

    Added integration tests for UserStalker#onEnable

--- src/test/groovy/it/fulminazzo/userstalker/UserStalkerIntegrationTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/UserStalkerIntegrationTest.groovy
@@ -1,6 +1,7 @@
 package it.fulminazzo.userstalker
 
-
+import it.fulminazzo.userstalker.command.USCommand
+import it.fulminazzo.userstalker.listener.PlayerListener
 import org.bukkit.Bukkit
 import org.bukkit.Server
 import org.bukkit.command.PluginCommand
@@ -31,6 +32,25 @@ class UserStalkerIntegrationTest extends Specification {
         server.pluginManager >> mockPluginManager
 
         if (Bukkit.server == null) Bukkit.server = server
+    }
+
+    def 'test full cycle load'() {
+        given:
+        def plugin = new UserStalker()
+
+        when:
+        plugin.onEnable()
+
+        then:
+        plugin.configuration != null
+        plugin.messages != null
+        plugin.apiClient != null
+        plugin.profileCache != null
+        plugin.getGUIManager() != null
+
+        and:
+        1 * mockCommand.setExecutor(_ as USCommand)
+        1 * mockPluginManager.registerEvents(_ as PlayerListener, _ as UserStalker)
     }
 
 }
