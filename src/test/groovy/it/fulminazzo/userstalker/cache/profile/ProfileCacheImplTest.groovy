739a0fdb53f8904cec00eea7f25115c4112015c6
user <fulminazzo21@gmail.com>
2025-05-29 18:25:28 +0200

    Moved getJsonFromURL tests to HttpUtils test suite

--- src/test/groovy/it/fulminazzo/userstalker/cache/profile/ProfileCacheImplTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/cache/profile/ProfileCacheImplTest.groovy
@@ -2,12 +2,11 @@ package it.fulminazzo.userstalker.cache.profile
 
 import com.google.gson.JsonObject
 import it.fulminazzo.fulmicollection.objects.Refl
-import it.fulminazzo.userstalker.cache.exception.CacheException
+import it.fulminazzo.userstalker.cache.utils.HttpUtils
 import it.fulminazzo.userstalker.utils.GsonUtils
 import spock.lang.Specification
 
 class ProfileCacheImplTest extends Specification {
-    private static final String ACTION = 'searching for userstalker repository'
 
     private ProfileCacheImpl cache
 
@@ -49,7 +48,10 @@ class ProfileCacheImplTest extends Specification {
     def 'test that fetchUserSkin of #jsonObject is as expected'() {
         given:
         def skinCache = Spy(TestProfileCache)
-        skinCache.getJsonFromURL(_ as String, _ as String) >> Optional.of(jsonObject)
+
+        and:
+        SpyStatic(HttpUtils)
+        HttpUtils.getJsonFromURL(_ as String, _ as String) >> Optional.of(jsonObject)
 
         when:
         def skin = skinCache.fetchUserSkin('Notch')
@@ -85,7 +87,7 @@ class ProfileCacheImplTest extends Specification {
         and:
         def cache = Spy(TestProfileCache)
         cache.getUserUUID(_ as String) >> Optional.of(UUID.randomUUID())
-        cache.getJsonFromURL(_ as String, _ as String) >> Optional.empty()
+        HttpUtils.getJsonFromURL(_ as String, _ as String) >> Optional.empty()
         cache.isInFetchBlacklist(_ as String) >> { args ->
             return blacklist
         }
@@ -136,57 +138,6 @@ class ProfileCacheImplTest extends Specification {
         then:
         fetchBlacklist().containsKey(username)
         !second.isPresent()
-    }
-
-    def 'test that getJsonFromURL returns valid Json'() {
-        given:
-        def url = 'https://api.github.com/repos/fulminazzo/userstalker'
-
-        when:
-        def json = cache.getJsonFromURL(url, ACTION).get()
-
-        then:
-        json.get('name').asString == 'userstalker'
-        json.get('full_name').asString == 'fulminazzo/userstalker'
-        json.get('private').asString == 'false'
-    }
-
-    def 'test that getJsonFromURL returns empty optional on 404'() {
-        given:
-        def url = 'https://api.github.com/repos/fulminazzo/notexisting'
-
-        when:
-        def json = cache.getJsonFromURL(url, ACTION)
-
-        then:
-        !json.isPresent()
-    }
-
-    def 'test that getJsonFromURL with invalid URL throws'() {
-        when:
-        cache.getJsonFromURL('invalid', ACTION)
-
-        then:
-        def e = thrown(CacheException)
-        e.message == 'Invalid URL provided: invalid'
-    }
-
-    def 'test that getJsonFromURL with general error throws'() {
-        when:
-        cache.getJsonFromURL('http://localhost', ACTION)
-
-        then:
-        def e = thrown(CacheException)
-        e.message == "ConnectException when $ACTION: Connection refused (Connection refused)"
-    }
-
-    def 'test that getJsonFromURL with invalid code throws'() {
-        when:
-        cache.getJsonFromURL('https://sessionserver.mojang.com/session/minecraft/profile/aa', ACTION)
-
-        then:
-        def e = thrown(CacheException)
-        e.message == "Invalid response code when $ACTION: 400"
     }
 
     def 'test that isInFetchBlacklist returns #expected for #time'() {
--- src/test/groovy/it/fulminazzo/userstalker/cache/utils/HttpUtilsTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/cache/utils/HttpUtilsTest.groovy
@@ -1,6 +1,60 @@
 package it.fulminazzo.userstalker.cache.utils
 
+import it.fulminazzo.userstalker.cache.exception.CacheException
 import spock.lang.Specification
 
 class HttpUtilsTest extends Specification {
+    private static final String ACTION = 'searching for userstalker repository'
+
+    def 'test that getJsonFromURL returns valid Json'() {
+        given:
+        def url = 'https://api.github.com/repos/fulminazzo/userstalker'
+
+        when:
+        def json = HttpUtils.getJsonFromURL(url, ACTION).get()
+
+        then:
+        json.get('name').asString == 'userstalker'
+        json.get('full_name').asString == 'fulminazzo/userstalker'
+        json.get('private').asString == 'false'
+    }
+
+    def 'test that getJsonFromURL returns empty optional on 404'() {
+        given:
+        def url = 'https://api.github.com/repos/fulminazzo/notexisting'
+
+        when:
+        def json = HttpUtils.getJsonFromURL(url, ACTION)
+
+        then:
+        !json.isPresent()
+    }
+
+    def 'test that getJsonFromURL with invalid URL throws'() {
+        when:
+        HttpUtils.getJsonFromURL('invalid', ACTION)
+
+        then:
+        def e = thrown(CacheException)
+        e.message == 'Invalid URL provided: invalid'
+    }
+
+    def 'test that getJsonFromURL with general error throws'() {
+        when:
+        HttpUtils.getJsonFromURL('http://localhost', ACTION)
+
+        then:
+        def e = thrown(CacheException)
+        e.message == "ConnectException when $ACTION: Connection refused (Connection refused)"
+    }
+
+    def 'test that getJsonFromURL with invalid code throws'() {
+        when:
+        HttpUtils.getJsonFromURL('https://sessionserver.mojang.com/session/minecraft/profile/aa', ACTION)
+
+        then:
+        def e = thrown(CacheException)
+        e.message == "Invalid response code when $ACTION: 400"
+    }
+
 }
