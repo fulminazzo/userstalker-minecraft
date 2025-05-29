cd807eaca93315e3918ac901c74f971408738712
user <fulminazzo21@gmail.com>
2025-05-29 18:50:09 +0200

    Created test suite for IPCacheImpl and added tests for fetchIPInfo method

---
+++ src/test/groovy/it/fulminazzo/userstalker/cache/ip/IPCacheImplTest.groovy
@@ -0,0 +1,36 @@
+package it.fulminazzo.userstalker.cache.ip
+
+import it.fulminazzo.userstalker.cache.domain.IPInfo
+import spock.lang.Specification
+
+import java.util.logging.Logger
+
+class IPCacheImplTest extends Specification {
+
+    private IPCacheImpl cache
+
+    void setup() {
+        cache = new IPCacheImpl(Logger.getLogger(getClass().simpleName))
+    }
+
+    def 'test that fetchIPInfo returns #expected for ip #ip'() {
+        when:
+        def info = cache.fetchIPInfo(ip).orElse(null)
+
+        then:
+        info == expected
+
+        where:
+        ip          || expected
+        'invalid'   || null
+        '24.48.0.1' || IPInfo.builder()
+                .ip('24.48.0.1')
+                .country('Canada')
+                .countryCode('CA')
+                .region('Quebec')
+                .city('Montreal')
+                .isp('Le Groupe Videotron Ltee')
+                .build()
+    }
+
+}
