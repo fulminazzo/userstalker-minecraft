0120bb8541025fc15a74603159906f7be5b2f156
user <fulminazzo21@gmail.com>
2025-05-29 18:11:52 +0200

    Created HttpUtils class

---
+++ src/main/java/it/fulminazzo/userstalker/cache/utils/HttpUtils.java
@@ -0,0 +1,11 @@
+package it.fulminazzo.userstalker.cache.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * A collection of utilities related to HTTP.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class HttpUtils {
+}
