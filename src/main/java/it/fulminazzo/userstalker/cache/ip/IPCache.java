dffab4998de39ae54fa7fdbea6f723ec022bcc02
user <fulminazzo21@gmail.com>
2025-05-29 18:31:27 +0200

    Created IPCache with lookupIPInfoAnd method

---
+++ src/main/java/it/fulminazzo/userstalker/cache/ip/IPCache.java
@@ -0,0 +1,25 @@
+package it.fulminazzo.userstalker.cache.ip;
+
+import it.fulminazzo.userstalker.cache.domain.IPInfo;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+
+/**
+ * A cache that stores information about an IP address lookup.
+ */
+public interface IPCache {
+
+    /**
+     * Looks up the IP address and executes the given function.
+     *
+     * @param ip     the ip address
+     * @param then   the function to execute in case it was found
+     * @param orElse the function to execute in case an error occurred
+     */
+    void lookupIPInfoAnd(final @NotNull String ip,
+                         final @NotNull Consumer<IPInfo> then,
+                         final @Nullable Runnable orElse);
+
+}
