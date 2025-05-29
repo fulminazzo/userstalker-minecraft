a66cba680254786c2d7cea52d76b322c5ca22645
user <fulminazzo21@gmail.com>
2025-05-29 18:29:51 +0200

    Created IPInfo class

---
+++ src/main/java/it/fulminazzo/userstalker/cache/domain/IPInfo.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.userstalker.cache.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Represents all the information about a given IP.
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public final class IPInfo {
+
+    private final String ip;
+
+    private final String continent;
+
+    private final String country;
+    private final String countryCode;
+
+    private final String region;
+
+    private final String city;
+
+    private final String isp;
+
+    private final boolean mobile;
+    private final boolean proxy;
+
+}
