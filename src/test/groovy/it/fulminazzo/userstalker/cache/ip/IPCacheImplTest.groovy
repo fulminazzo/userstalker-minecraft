d5b77db3f8384a199706a2669bf9f557c15455f0
user <fulminazzo21@gmail.com>
2025-05-29 18:54:57 +0200

    Added positive tests for lookupIPInfoAnd method

--- src/test/groovy/it/fulminazzo/userstalker/cache/ip/IPCacheImplTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/cache/ip/IPCacheImplTest.groovy
@@ -13,6 +13,37 @@ class IPCacheImplTest extends Specification {
         cache = new IPCacheImpl(Logger.getLogger(getClass().simpleName))
     }
 
+    def 'test that lookupIPInfoAnd runs the consumer for ip #ip'() {
+        given:
+        def info = IPInfo.builder()
+                .ip('127.0.0.1')
+                .country('Canada')
+                .countryCode('CA')
+                .region('Quebec')
+                .city('Montreal')
+                .isp('Le Groupe Videotron Ltee')
+                .build()
+
+        and:
+        cache.cache.put('127.0.0.1', info)
+
+        and:
+        def run = false
+
+        when:
+        cache.lookupIPInfoAnd(ip, i -> run = true, () -> { })
+
+        then:
+        run == true
+
+        where:
+        ip << [
+                '127.0.0.1',
+                '24.48.0.1',
+                'invalid'
+        ]
+    }
+
     def 'test that fetchIPInfo returns #expected for ip #ip'() {
         when:
         def info = cache.fetchIPInfo(ip).orElse(null)
