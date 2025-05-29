49606512f1414d78db5ab90d6dd6f0983d5f137d
user <fulminazzo21@gmail.com>
2025-05-29 18:45:15 +0200

    Added newCache function

--- src/main/java/it/fulminazzo/userstalker/cache/ip/IPCache.java
+++ src/main/java/it/fulminazzo/userstalker/cache/ip/IPCache.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
+import java.util.logging.Logger;
 
 /**
  * A cache that stores information about an IP address lookup.
@@ -21,5 +22,15 @@ public interface IPCache {
     void lookupIPInfoAnd(final @NotNull String ip,
                          final @NotNull Consumer<IPInfo> then,
                          final @Nullable Runnable orElse);
+
+    /**
+     * Creates a new IP cache.
+     *
+     * @param logger the logger
+     * @return the ip cache
+     */
+    static @NotNull IPCache newCache(final @NotNull Logger logger) {
+        return new IPCacheImpl(logger);
+    }
 
 }
